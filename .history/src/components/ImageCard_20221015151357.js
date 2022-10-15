1f9bbddd937e456d472a125687e602400d72f90a
EgorSherstnev <svarog91@list.ru>
2022-10-15 17:00:51 +0300

    add final up in the app

--- .history/src/components/ImageCard_20221015151357.js
+++ .history/src/components/ImageCard_20221015151357.js
@@ -4,8 +4,6 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { spans: 0 };
-
         this.imageRef = React.createRef();
     }
 
@@ -14,18 +12,14 @@ class ImageCard extends React.Component {
     }
 
     setSpans = () => {
-        const height = this.imageRef.current.clientHeight;
-
-        const spans = Math.ceil(height / 10);
-
-        this.setState({ spans });
+        console.log(this.imageRef.current.clientHeight);
     }
 
     render() {
         const {description, urls} = this.props.image;
 
         return (
-            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+            <div>
                 <img 
                     ref={this.imageRef}
                     alt={description}
